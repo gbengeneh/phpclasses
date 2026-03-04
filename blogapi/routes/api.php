6ff8965b6ebe54cde3c0baf12289daefd9e9a8f7
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2026-03-04 16:24:18 +0100

    added supdated

--- blogapi/routes/api.php
+++ blogapi/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../controllers/AuthController.php';
+require_once __DIR__ . '/../controllers/CategoryController.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -26,6 +27,7 @@ if($method === 'OPTIONS'){
     exit();
 }
 $authController = new AuthController();
+$categoryController = new CategoryController();
 
 if($request[0] === 'register' && $method === 'POST'){
     $data = json_decode(file_get_contents("php://input"), true);
@@ -33,6 +35,26 @@ if($request[0] === 'register' && $method === 'POST'){
 } elseif($request[0] === 'login' && $method === 'POST'){
     $data = json_decode(file_get_contents("php://input"), true);
     $authController->login($data);
+}elseif($request[0]=== 'categories'){
+    if($method==='GET'){
+        if(isset($request[1])){
+            $categoryController->getCategory($request[1]);
+        }else{
+            $categoryController->getAllCategories();
+        }
+    }elseif($method==='POST'){
+        $data = json_decode(file_get_contents("php://input"), true);
+        $categoryController->createCategory($data);
+    }elseif($method==='PUT' && isset($request[1])){
+        $data = json_decode(file_get_contents("php://input"), true);
+        $categoryController->updateCategory($request[1], $data);
+    }elseif($method==='DELETE' && isset($request[1])){
+        $categoryController->deleteCategory($request[1]);
+    } else {
+        http_response_code(405);
+        echo json_encode(['message' => 'method not allowed']);
+    }
+        
 } else {
     http_response_code(404);
     echo json_encode(['message' => 'Endpoint not found']);
