eec3af75e250c0117b6638bba245879e501fd43b
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2026-03-16 16:13:24 +0100

    added updates

--- blogapi/routes/api.php
+++ blogapi/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../controllers/CategoryController.php';
+require_once __DIR__ . '/../controllers/PostController.php';
 
 
 header("Access-Control-Allow-Origin: *");
@@ -30,6 +31,7 @@ if ($method === 'OPTIONS') {
 
 $authController = new AuthController();
 $categoryController = new CategoryController();
+$postController = new PostController();
 
 if($request[0] === 'register' && $method === 'POST'){
     $data = json_decode(file_get_contents("php://input"), true);
@@ -56,7 +58,24 @@ if($request[0] === 'register' && $method === 'POST'){
         http_response_code(405);
         echo json_encode(['message' => 'method not allowed']);
     }
-        
+}elseif ($request[0] === 'posts') {
+    if ($method === 'GET') {
+        if (isset($request[1])) {
+            $postController->getPost($request[1]);
+        } else {
+            $postController->getAllPosts();
+        }
+    } elseif ($method === 'POST') {
+        $postController->createPost($_POST);
+    } elseif ($method === 'PUT' && isset($request[1])) {
+        parse_str(file_get_contents("php://input"), $put_vars);
+        $postController->updatePost($request[1], $put_vars);
+    } elseif ($method === 'DELETE' && isset($request[1])) {
+        $postController->deletePost($request[1]);
+    } else {
+        http_response_code(405);
+        echo json_encode(['message' => 'Method not allowed']);
+    }
 } else {
     http_response_code(404);
     echo json_encode(['message' => 'Endpoint not found']);
